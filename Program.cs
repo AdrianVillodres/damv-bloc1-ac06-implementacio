74d8a838db1446f5d23138bbc304c85786113e7d
AdrianVillodres <adrian.villodres.7e8@itb.cat>
2025-10-16 12:35:27 +0200

    code done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.VisualBasic;
+using System;
+using System.ComponentModel;
+using System.Security.Cryptography;
+namespace CelsiusToKelvin
+{
+    public class CelsiusToKelvin
+    {
+        public static void Main()
+        {
+            // PRECONDITION: The user is prompted to input a temperature in Celsius.
+
+            const string MSG_INPUT = "Enter temperature in Celsius: ";
+            const string MSG_RESULT = "Temperature in Kelvin: ";
+
+            float celsius;
+            float kelvin;
+
+            Console.Write(MSG_INPUT);
+            celsius = float.Parse(Console.ReadLine());
+            kelvin = celsius + 273.15f;
+            Console.WriteLine(MSG_RESULT + kelvin);
+
+
+            // POSTCONDITION: 'kelvin' contains the converted temperature, and it is 
+            // displayed.
+        }
+    }
+}

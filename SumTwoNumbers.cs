9f12e5e0958e0885a628583b500ecd2f579cca81
AdrianVillodres <adrian.villodres.7e8@itb.cat>
2025-10-16 10:32:55 +0200

    initial commit

---
+++ SumTwoNumbers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace SumTwoNumbers
+{
+    public class SumTwoNumbers
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

1b9ff59b6e72975d188b3aee0fc00c9bfdac6c2e
AdrianVillodres <adrian.villodres.7e8@itb.cat>
2025-10-21 19:20:54 +0200

    code done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Security.Cryptography;
+namespace EuroToDollar
+{
+    public class EuroToDollar
+    {
+        public static void Main()
+        {
+            // PRECONDITION: The user is prompted to input an amount in euros.
+
+            const float EXCHANGE_RATE = 1.12f;
+            const string MSG_INPUT = "Enter amount in euros: ";
+            const string MSG_RESULT = "Amount in dollars: ";
+
+            float euros;
+            float dollars;
+
+            Console.Write(MSG_INPUT);
+            euros = float.Parse(Console.ReadLine());
+            dollars = euros * EXCHANGE_RATE;
+            Console.WriteLine(MSG_RESULT + dollars);
+
+            // POSTCONDITION: 'dollars' contains the converted amount, and it is displayed.
+
+        }
+    }
+}

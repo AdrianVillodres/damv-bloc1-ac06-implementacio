d8b38f2af6c5864718e898763972bfd91c5eda29
AdrianVillodres <adrian.villodres.7e8@itb.cat>
2025-10-21 19:13:48 +0200

    code done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace RectangleArea
+{
+    public class RectangleArea
+    {
+        public static void Main()
+        {
+            // PRECONDITION: The user is prompted to input the width and height of a rectangle.
+
+            const string MSG_INPUT_WIDTH = "Enter the width of the rectangle: ";
+            const string MSG_INPUT_HEIGHT = "Enter the height of the rectangle: ";
+            const string MSG_RESULT = "The area of the rectangle is: ";
+
+            float width;
+            float height;
+            float area;
+
+            Console.Write(MSG_INPUT_WIDTH);
+            width = float.Parse(Console.ReadLine());
+            Console.Write(MSG_INPUT_HEIGHT);
+            height = float.Parse(Console.ReadLine());
+            area = width * height;
+            Console.WriteLine(MSG_RESULT + area);
+
+
+            // POSTCONDITION: 'area' contains the product of 'width' and 'height', and it is displayed.
+        }
+    }
+}
